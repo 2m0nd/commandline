5b3c9ee5f31014db60d24126e988c30aeac4fe9b
Dmitry Vlasov <d.vlasov@tochka.com>
2017-06-19 18:02:16 +0500

    Bug fix enum parsing when many options cases.

--- tests/CommandLine.Tests/Unit/Core/InstanceChooserTests.cs
+++ tests/CommandLine.Tests/Unit/Core/InstanceChooserTests.cs
@@ -22,6 +22,7 @@ namespace CommandLine.Tests.Unit.Core
                 types,
                 arguments,
                 StringComparer.Ordinal,
+                true,
                 CultureInfo.InvariantCulture,
                 Enumerable.Empty<ErrorType>());
         }
